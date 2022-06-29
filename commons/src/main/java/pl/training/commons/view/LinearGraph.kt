c8bfff7937b4c99b695ff52d975eaad0cce025f6
landrzejewski <landrzejewski.poczta@gmail.com>
2022-06-29 14:41:08 +0200

    x

--- commons/src/main/java/pl/training/commons/view/LinearGraph.kt
+++ commons/src/main/java/pl/training/commons/view/LinearGraph.kt
@@ -2,12 +2,21 @@ package pl.training.commons.view
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 
 class LinearGraph(context: Context, attributeSet: AttributeSet): View(context, attributeSet) {
 
     private var points = emptyList<Point>()
+    private fun getPaint(): Paint {
+        val paint = Paint()
+        paint.style = Paint.Style.STROKE
+        paint.color = Color.LTGRAY
+        paint.strokeWidth = 10.0F
+        return paint
+    }
 
     fun draw(points: List<Point>) {
         this.points = points
